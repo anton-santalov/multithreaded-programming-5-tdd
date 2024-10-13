2e0157c7c71d75ee2f489a1264e618894f669491
Anton Santalov <anton.santalov1995@gmail.com>
2024-10-13 18:05:58 +0400

    add implemented.

--- src/main/java/com/antonsantalov/PhoneBook.java
+++ src/main/java/com/antonsantalov/PhoneBook.java
@@ -1,7 +1,15 @@
 package com.antonsantalov;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class PhoneBook {
+    private final Map<String, String> contacts = new ConcurrentHashMap<>();
+    private final Map<String, String> reversedContacts = new ConcurrentHashMap<>();
+
     public int add(String nickname, String phoneNumber) {
-        return 0;
+        contacts.put(nickname, phoneNumber);
+        reversedContacts.put(phoneNumber, nickname);
+        return contacts.size();
     }
 }
